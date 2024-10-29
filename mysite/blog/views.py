588096961dad0a0d3b18ec0a5c079dc306d8a43f
Iraqez <vladimir.gumenyuk@gmail.com>
2024-10-29 13:40:22 +0200

    first commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+from django.http import Http404
+
+
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(
+		Post,
+		status=Post.Status.PUBLISHED,
+		slug=post,
+		publish__year=year,
+		publish__month=month,
+		publish__day=day,
+  	)
+	return render(
+		request,
+		'blog/post/detail.html',
+		{'post': post}
+	)
+
+def post_list(request):
+	posts = Post.published.all()
+	return render(
+		request,
+		'blog/post/list.html',
+		{'posts': posts}
+	)
+ 
