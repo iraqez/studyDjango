90b7bc89af16f88b532751bbb552c050c0a5ec5a
Iraqez <vladimir.gumenyuk@gmail.com>
2024-10-31 00:13:39 +0200

    update blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,9 @@
-# from django.db import models
-# from django.utils import timezone
-# from django.contrib.auth.models import User
 from django.db.models.functions import Now
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -17,34 +15,41 @@ class PublishedManager(models.Manager):
 class Post(models.Model):
     
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = 'DF', 'Чорновик'
+        PUBLISHED = 'PB', 'Публікація'
     
         
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    title = models.CharField(max_length=250, verbose_name='Заголовок',)
+    slug = models.SlugField(max_length=250, unique_for_date='publish', verbose_name='Посилання',)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='blog_posts',
+        verbose_name='Автор',
     )
-    body = models.TextField()
-    publish = models.DateTimeField(db_default=Now())
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    body = models.TextField(verbose_name='Опис',)
+    publish = models.DateTimeField(db_default=Now(), verbose_name='Публікація',)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Створено',)
+    updated = models.DateTimeField(auto_now=True, verbose_name='Оновлено',)
     status = models.CharField(
         max_length=2,
         choices=Status, 
-        default=Status.DRAFT
+        default=Status.DRAFT,
+        verbose_name='Статус',
         )
     objects = models.Manager()
     published = PublishedManager()
+    
+    tags = TaggableManager()
     
     class Meta:
         ordering = ['-publish',]
         indexes = [
             models.Index(fields=['-publish']),
         ]
+
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Пости'
 
     def __str__(self):
         return self.title
@@ -59,3 +64,29 @@ class Post(models.Model):
                 self.slug,
             ]
         )
+        
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост',
+    )
+    name = models.CharField(max_length=80, verbose_name="Ім'я",)
+    email = models.EmailField(verbose_name='Електронна пошта',)
+    body = models.TextField(verbose_name='Коментар',)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення',)
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата редагування',)
+    active = models.BooleanField(default=True, verbose_name='Активний',)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+        
+        verbose_name = 'Коментар'
+        verbose_name_plural = 'Коментарі'
+    
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
